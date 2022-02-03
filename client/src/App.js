1e2e6bc8ceb363ed9b44f760796df86f9cea57df
Cyndexz <cynthiahernandez1108@hotmail.com>
2022-02-02 21:30:05 -0600

    Revamp skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 
 function App() {
     return(
         <div>
-            <h1>Hello gamers</h1>
+            <h1>hello</h1>
         </div>
     );
 }
